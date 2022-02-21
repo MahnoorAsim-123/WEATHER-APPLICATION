b04a677c2dce088b011cb621df936ff71327a8ff
Mahnoor Asim <92971645+MahnoorAsim-123@users.noreply.github.com>
2022-02-21 11:19:03 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import Weather from "./weather";
+
+function App() {
+  return (
+     <Weather/>
+  );
+}
+
+export default App;
+
+// https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=8339e1e8e05bdd978bd819f34db35976&units=metric
